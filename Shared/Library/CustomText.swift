3924a51720b6a5e5dceb1bdfafeb81b32c3c8af4
hajdinim <hajdinim@uni.coventry.ac.uk>
2020-10-08 06:41:27 +0100

    cleaner code

--- Shared/Library/CustomText.swift
+++ Shared/Library/CustomText.swift
@@ -6,7 +6,7 @@ struct LibraryTextContent: LibraryContentProvider {
     func subtitleText(base: Text) -> [LibraryItem] {
         LibraryItem(
             base.timeText(),
-            title: "secondary textt"
+            title: "secondary text"
         )
     }
     @LibraryContentBuilder
