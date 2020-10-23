5a061f368378f62ba2710bd5f5a5dc82b25a4aa2
hajdinim <hajdinim@uni.coventry.ac.uk>
2020-10-23 14:35:51 +0100

    cleaner code

--- Shared/Library/CustomText.swift
+++ Shared/Library/CustomText.swift
@@ -13,7 +13,7 @@ struct LibraryTextContent: LibraryContentProvider {
     func titleText(base: Text) -> [LibraryItem] {
         LibraryItem(
             base.primaryText(),
-            title: "Primary text"
+            title: "Primary textt"
         )
     }
 }
